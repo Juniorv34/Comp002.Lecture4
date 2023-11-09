3b40ae6f221d3d7c4df3f8a11229953eb86f6b1b
junior vargas <juniorvargascovarrub@my.whccd.edu>
2023-11-08 20:22:41 -0800

    I made the circle circumference algorithm

--- script.js
+++ script.js
@@ -4,3 +4,9 @@ function test(n){
 }
 //test (" Junior");
 console.log(test(" Junior2"));
+let diameter = 26
+if (diameter > 0) {
+    let pi = 3.14;
+    let circumference = pi * diameter;
+    console.log("The circumference is: " + circumference);
+}
